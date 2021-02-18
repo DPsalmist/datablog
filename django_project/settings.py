dc9823e109e48ad1afbaf46edc956d7c824305fc
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-02-18 10:24:27 +0100

    Added whitenoise

--- django_project/settings.py
+++ django_project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'BASE_DIR / staticfiles'
+
+STATICFILES_DIR = 'BASE_DIR /static'
