21cf909c67a7b39b81342d2e3cff4a058315c8be
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-02-22 02:41:18 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,15 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#<<<<<<< HEAD
-
 STATIC_ROOT = 'BASE_DIR / staticfiles'
-
 STATICFILES_DIR = 'BASE_DIR /static'
 
 # Configure Django App for Heroku.
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
 =======
 #STATIC_ROOT = 'BASE_DIR / staticfiles'
 
